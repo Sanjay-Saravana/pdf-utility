08e7b163f41c4729b43c1b6b98f1f6e533f3bc47
Sanjay-Saravana <softdevsanjay@gmail.com>
2025-07-24 22:51:45 +0530

    add backend url

--- frontend/src/BackendContext.jsx
+++ frontend/src/BackendContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext } from 'react';
 const BackendContext = createContext();
 
 export const BackendProvider = ({ children }) => {
-  const BACKEND_URL = ''
+  const BACKEND_URL = 'https://pdf-utility-qldt.onrender.com'
   return (
     <BackendContext.Provider value={BACKEND_URL}>
       {children}
